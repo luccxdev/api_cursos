7cb80e9bc85b9fdaa3c73f3e2e56309ae5195ccf
luccxdev <luccasxdev@gmail.com>
2025-12-14 23:39:10 -0300

    feat: implementa listagem de cursos GET /cursos

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 cursos = []
 
+
+
+@app.route('/cursos', methods=['GET'])
+def listar_cursos():
+    return jsonify(cursos), 200
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
