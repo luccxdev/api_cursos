c616bf3eac54f2fb17ac36f7017aea476c39805f
luccxdev <luccasxdev@gmail.com>
2025-12-14 23:40:09 -0300

    feat: implementa operações CRUD POST, GET, PUT, DELETE /cursos

--- app.py
+++ app.py
@@ -9,5 +9,36 @@ cursos = []
 def listar_cursos():
     return jsonify(cursos), 200
 
+@app.route('/cursos', methods=['POST'])
+def criar_curso():
+    novo_curso = request.get_json()
+    novo_curso['id'] = len(cursos) + 1
+    cursos.append(novo_curso)
+    return jsonify(novo_curso), 201
+
+@app.route('/cursos/<int:id>', methods=['GET'])
+def obter_curso(id):
+    for curso in cursos:
+        if curso['id'] == id:
+            return jsonify(curso), 200
+    return jsonify({'erro': 'Curso não encontrado'}), 404
+
+@app.route('/cursos/<int:id>', methods=['PUT'])
+def atualizar_curso(id):
+    for curso in cursos:
+        if curso['id'] == id:
+            dados = request.get_json()
+            curso.update(dados)
+            return jsonify(curso), 200
+    return jsonify({'erro': 'Curso não encontrado'}), 404
+
+@app.route('/cursos/<int:id>', methods=['DELETE'])
+def deletar_curso(id):
+    for i, curso in enumerate(cursos):
+        if curso['id'] == id:
+            cursos.pop(i)
+            return jsonify({'mensagem': 'Curso deletado'}), 200
+    return jsonify({'erro': 'Curso não encontrado'}), 404
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
